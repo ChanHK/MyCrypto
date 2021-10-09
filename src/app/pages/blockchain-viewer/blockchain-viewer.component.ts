3d8752bfb4722529e320d672c4478d07f88afb39
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-09 13:26:52 +0800

    blockchain-viewer done

--- src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
+++ src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
@@ -12,13 +12,30 @@ export class BlockchainViewerComponent implements OnInit {
   public selectedBlock: any = null;
 
   constructor(private blockchainService: BlockchainService) {
-    this.blocks = blockchainService.getBlocks();
+    this.blocks = blockchainService.blockchainInstance.chain;
     this.selectedBlock = this.blocks[0];
   }
 
   ngOnInit(): void {}
 
-  showTransactions(block: any) {
+  showTransactions(block: Block) {
     this.selectedBlock = block;
+    return false;
   }
+
+  blockHasTx = (block: Block) => {
+    return block.transactions.length > 0;
+  };
+
+  selectedBlockHasTx = () => {
+    return this.blockHasTx(this.selectedBlock);
+  };
+
+  isSelectedBlock = (block: Block) => {
+    return this.selectedBlock === block;
+  };
+
+  getBlockNumber = (block: Block) => {
+    return this.blocks.indexOf(block) + 1;
+  };
 }
