bbb1948b953c43a5334de461f5afa0673e513cde
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-10 16:00:53 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-const routes: Routes = [{ path: '', component: BlockchainViewerComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: BlockchainViewerComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '**', component: NoPageFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
