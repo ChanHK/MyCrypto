3b4444f8dc756a9a74b50821868dbf680877dd0a
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-14 21:13:22 +0800

    Update create-transaction.component.ts

--- src/app/pages/create-transaction/create-transaction.component.ts
+++ src/app/pages/create-transaction/create-transaction.component.ts
@@ -1,32 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Transaction } from 'src/app/classes/transaction.model';
-import { BlockchainService, IWalletKey } from 'src/app/services/blockchain.service';
+import {
+  BlockchainService,
+  IWalletKey,
+} from 'src/app/services/blockchain.service';
 
 @Component({
   selector: 'app-create-transaction',
   templateUrl: './create-transaction.component.html',
-  styleUrls: ['./create-transaction.component.scss']
+  styleUrls: ['./create-transaction.component.scss'],
 })
 export class CreateTransactionComponent implements OnInit {
-
   public newTx = new Transaction();
   public ownWalletKey: IWalletKey;
 
-  constructor(private blockchainService: BlockchainService, private router: Router) {
-    this.newTx = new Transaction();
+  constructor(
+    private blockchainService: BlockchainService,
+    private router: Router
+  ) {
+    // this.newTx = new Transaction();
     this.ownWalletKey = blockchainService.walletKeys[0];
   }
 
   ngOnInit(): void {
+    this.newTx = new Transaction();
   }
 
   createTransaction() {
-    const newTx = this.newTx;
-
     // Set the FROM address and sign the transaction
-    newTx.fromAddress = this.ownWalletKey.publicKey;
-    newTx.signTransaction(this.ownWalletKey.keyObj);
+    this.newTx.fromAddress = this.ownWalletKey.publicKey;
+    this.newTx.signTransaction(this.ownWalletKey.keyObj);
 
     try {
       this.blockchainService.addTransaction(this.newTx);
